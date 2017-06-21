19eb47cba19a26f763632305accc68e7d82aa4e7
abhishekw <waichal.abhishek@gmail.com>
2017-06-21 08:06:02 -0700

    add: Login component and login service

--- ui-src/src/app/components/login/login.component.ts
+++ ui-src/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService} from '../../services/validate.service';
+import { AuthService} from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService,
+              private flashMessageService: FlashMessagesService,
+              private authService: AuthService,
+              private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(){
+  }
+
+  onLoginSubmit(){
+
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+
+    //login
+    this.authService.authenticateUser(user).subscribe( data => {
+
+      if(data.success){
+      this.authService.storeUserData(data.msg, data.name);
+      this.flashMessageService.show ('LOGGED IN',
+                        { cssClass:'alert-success', timeout: 3000 });
+      this.router.navigate(['/profile']);
+    }else{
+      this.flashMessageService.show ('Sign In Failed: Wrong username and/or password!',
+                        { cssClass:'alert-danger', timeout: 3000 });
+      //this.router.navigate(['/login']);
+    }
+
+    });
   }
 
 }
